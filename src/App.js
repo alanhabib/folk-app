2a03ead18638531934910129d56d8a66ce304444
alan habib <alanhabib@h109-225-80-225.cust.a3fiber.se>
2018-12-04 22:56:12 +0100

    finished task about choosing one of 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./Users.css";
+
 
 class App extends Component {
+  state = {
+    users: [],
+    selectedUser: -1
+  };
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+  getAllUsers = () => {
+    // console.log(id)
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ users: data });
+      })
+      .catch(error => console.log(error));
+  };
+
+  getUser = id => {
+    this.setState({ selectedUser: id });
+  };
+
   render() {
+    const listOfUsers = this.state.users.slice(0, 5).map(user => (
+      <p
+        key={user.id}
+        className="userBox"
+        onClick={this.getUser.bind(this, user.id)}
+      >
+        {user.id}
+        {user.name}
+      </p>
+    ));
+
+    if (this.state.selectedUser > -1) {
+      const userFacts = this.state.users.slice(0, 5).map(facts => {
+        if (this.state.selectedUser === facts.id) {
+          return (
+            <p key={facts.id}>
+              {facts.name}
+              {facts.username}
+              {facts.email}
+              {facts.address.street}
+              {facts.company.name}
+            </p>
+          );
+        }
+      });
+      return userFacts;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {listOfUsers}
       </div>
     );
   }
